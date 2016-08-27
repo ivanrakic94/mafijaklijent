9ae1b9aae78a741f15e59fd0eb7478bab9dd24ad
Gordan <Gordan@Thekgiga>
2016-08-28 01:04:56 +0200

    Odradjena konekcija na bazu. Registrovanje i prijavljivanje igraca.Sredjeni Exceptioni(server nije pokrenut,server pokrenut vise puta, kada igrac unese neki od nedozvoljenih znakova... ). prepravljen gui.

--- src/MafijaKlijent.java
+++ src/MafijaKlijent.java
@@ -6,169 +6,196 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+import javax.swing.*;
 
 public class MafijaKlijent {
-	
+
 	static Socket soketZaKomunikaciju = null;
-	static Socket soketZaKom=null;
+	static Socket soketZaKom = null;
 	static BufferedReader ulazOdServera;
 	static PrintStream izlazKaServeru;
-	//oznacava da li je igrac i dalje u igri
+	// oznacava da li je igrac i dalje u igri
 	static boolean status = true;
-	//oznaka za kraj partije
+	// oznaka za kraj partije
 	static boolean kraj = false;
-	//prozor za unosenje imena
+	// prozor za unosenje imena
 	static PocetniProzor pp;
-	//glavni prozor u kome se igra
+	// glavni prozor u kome se igra
 	static GlavniProzor gp;
-	//prozor za izbor nove partije ili kraj
+	// prozor za izbor nove partije ili kraj
 	static ProzorZaKraj pk;
-	
+
 	public static void main(String[] args) {
 		try {
 			soketZaKomunikaciju = new Socket("localhost", 2906);
-			soketZaKom=new Socket("localhost",2907);
+			soketZaKom = new Socket("localhost", 2907);
 			ulazOdServera = new BufferedReader(new InputStreamReader(soketZaKomunikaciju.getInputStream()));
 			izlazKaServeru = new PrintStream(soketZaKomunikaciju.getOutputStream());
-		
-		//beskonacna petlja da bi mogla da se bira nova partija neogranicen br puta
-		while (true) {
-			pp = new PocetniProzor();
-			pp.setVisible(true);
-			
-			//ova petlja sluzi da zaustavi izvrsavanje dok igrac ne pritisne dugme za unos imena
-			while (!pp.flag) {
-				System.out.println();
-			}
-			//kada igrac pritisne dugme onda se uzima ime
-			izlazKaServeru.println(pp.vratiIme());
-			gp = new GlavniProzor();
-			
-			gp.ime = pp.vratiIme();
-			
-			//dobijanje svih igraca od servera
-			String[] igraci = new String[6];
-			for (int i = 0; i < 6; i++) {
-				igraci[i] = ulazOdServera.readLine();
-			}
-			pp.dispose();
-			
-			gp.setVisible(true);
-		
-	
-			//ispisuje igrace u listu i dodaje njihova imena na radio button-e
-			gp.ispisiIgrace(igraci);
-			
-			//dobijanje uloge od servera
-			String uloga = ulazOdServera.readLine();
-			new Thread(new ChatNitZaSve(gp, pp.vratiIme(), soketZaKom,uloga)).start();
-			new Thread(new ChatNitZaSveSlanjePoruka(gp, pp.vratiIme(), soketZaKom)).start();
-			gp.ispisiUlogu(uloga);
-			
-			String ubice = ulazOdServera.readLine();
-			gp.ispisiUbice(ubice);
-			
-			//ova petlja se vrti dok god traje partija
-			while(!kraj) {
-			//ako igrac nije ubijen/izbacen
-			if(status) {
-			//pravi promene u glavnom prozoru u zavisnosti od uloge i salje serveru izbore
-			switch (uloga) {
-			case "Ubica":
-				gp.ulogaUbice();
-				while (!gp.ulogaFlag) {
+
+			// beskonacna petlja da bi mogla da se bira nova partija neogranicen
+			// br puta
+			while (true) {
+				pp = new PocetniProzor();
+				pp.setVisible(true);
+				//vrti se u while dok se ne obavi proces registracije/logovanja
+				while (!pp.uspesnoUlogovan) {
+					// ova petlja sluzi da zaustavi izvrsavanje dok igrac ne
+					// pritisne dugme za unos imena
+					while (!pp.flag) {
+						System.out.println("");
+					}
+					// kada igrac pritisne dugme onda se uzima ime
+
+					String username = pp.vratiIme();
+					String password = pp.vratiPassword();
+					String izlaz = username + ";" + password;
+
+					// nalepljuje string koji nam govori da treba da ubacimo
+					// novog igraca u bazu
+					if (pp.rdbtnRegistrujMe.isSelected()) {
+						izlaz += ";REGISTRUJ";
+					}
+					izlazKaServeru.println(izlaz);
+					if (ulazOdServera.readLine().equals("OK")) {
+						if (pp.rdbtnRegistrujMe.isSelected()) {
+							pp.uspesnoUlogovan = true;
+							JOptionPane.showMessageDialog(pp, "Uspesno ste se registrovali. Sacekajte ostale igrace...");
+						} else {
+							pp.uspesnoUlogovan = true;
+							JOptionPane.showMessageDialog(pp, "Uspesno ste se prijavili. Sacekajte ostale igrace...");
+						}
+					} else {
+						JOptionPane.showMessageDialog(pp, "Greska.Pokusajte ponovo.");
+						pp.obrisiPolja();
+
+					}
+
+					gp = new GlavniProzor();
+					gp.ime = username;
+				}
+				// dobijanje svih igraca od servera
+				String[] igraci = new String[6];
+				for (int i = 0; i < 6; i++) {
+					igraci[i] = ulazOdServera.readLine();
+				}
+				pp.dispose();
+
+				gp.setVisible(true);
+
+				// ispisuje igrace u listu i dodaje njihova imena na radio
+				// button-e
+				gp.ispisiIgrace(igraci);
+
+				// dobijanje uloge od servera
+				String uloga = ulazOdServera.readLine();
+				new Thread(new ChatNitZaSve(gp, pp.vratiIme(), soketZaKom, uloga)).start();
+				new Thread(new ChatNitZaSveSlanjePoruka(gp, pp.vratiIme(), soketZaKom)).start();
+				gp.ispisiUlogu(uloga);
+
+				String ubice = ulazOdServera.readLine();
+				gp.ispisiUbice(ubice);
+
+				// ova petlja se vrti dok god traje partija
+				while (!kraj) {
+					// ako igrac nije ubijen/izbacen
+					if (status) {
+						// pravi promene u glavnom prozoru u zavisnosti od uloge
+						// i salje serveru izbore
+						switch (uloga) {
+						case "Ubica":
+							gp.ulogaUbice();
+							while (!gp.ulogaFlag) {
+								System.out.println();
+							}
+							izlazKaServeru.println(gp.vratiIzborZaUlogu());
+							break;
+						case "Lekar":
+							gp.ulogaLekara();
+							while (!gp.ulogaFlag) {
+								System.out.println();
+							}
+							izlazKaServeru.println(gp.vratiIzborZaUlogu());
+							break;
+						case "Policajac":
+							gp.ulogaPolicajca();
+							while (!gp.ulogaFlag) {
+								System.out.println();
+							}
+							izlazKaServeru.println(gp.vratiIzborZaUlogu());
+							gp.odgovoriPolicajcu(ulazOdServera.readLine());
+							break;
+						default:
+							gp.ulogaGradjanina();
+							break;
+						}
+					}
+
+					// obavestava da li je neko ubijen i menja sadrzaj prozora
+					// za glasanje i izbacuje iz izbora ubijenog
+					gp.obavesti(ulazOdServera.readLine());
+
+					// dobija poruku od servera da li je igrac izbacen/ubijen
+					if (!status) {
+						ulazOdServera.readLine();
+					} else {
+						if (ulazOdServera.readLine().equals("izbacen")) {
+							gp.obavestiOIspadanju();
+							status = false;
+						}
+					}
+
+					// dobija poruku od servera da li je partija gotova
+					if (ulazOdServera.readLine().equals("kraj")) {
+						kraj = true;
+						continue;
+					}
+
+					// salje serveru odgovor za izbacivanje
+					if (status) {
+						while (!gp.izbacivanjeFlag) {
+							System.out.println();
+						}
+						izlazKaServeru.println(gp.vratiIzborZaIzbacivanje());
+					}
+
+					// dobija odgovor ko je izbacen
+					gp.obavestiOIzglasavanju(ulazOdServera.readLine());
+
+					// dobija poruku da li je igrac izbacen
+					if (ulazOdServera.readLine().equals("izbacen")) {
+						gp.obavestiOIspadanju();
+						status = false;
+					}
+
+					// dobija poruku da li je sad kraj partije
+					if (ulazOdServera.readLine().equals("kraj")) {
+						kraj = true;
+					}
+					// ako nije, sve ispocetka
+				}
+
+				// ako jeste kraj partije, izbaci prozor za ponovno biranje
+				// partije
+				pk = new ProzorZaKraj();
+				pk.setVisible(true);
+				// ispisi ko je pobedio
+				pk.ispisi(ulazOdServera.readLine());
+				gp.dispose();
+				while (!pk.flag) {
 					System.out.println();
 				}
-				izlazKaServeru.println(gp.vratiIzborZaUlogu());
-				break;
-			case "Lekar":
-				gp.ulogaLekara();
-				while (!gp.ulogaFlag) {
-					System.out.println();
-				}
-				izlazKaServeru.println(gp.vratiIzborZaUlogu());
-				break;
-			case "Policajac":
-				gp.ulogaPolicajca();
-				while (!gp.ulogaFlag) {
-					System.out.println();
-				}
-				izlazKaServeru.println(gp.vratiIzborZaUlogu());
-				gp.odgovoriPolicajcu(ulazOdServera.readLine());
-				break;
-			default:
-				gp.ulogaGradjanina();
-				break;
+				pk.flag = false;
+				pk.dispose();
 			}
-			}
-			
-			//obavestava da li je neko ubijen i menja sadrzaj prozora za glasanje i izbacuje iz izbora ubijenog
-			gp.obavesti(ulazOdServera.readLine());
-			
-				//dobija poruku od servera da li je igrac izbacen/ubijen
-				if (!status) {
-					ulazOdServera.readLine();
-				} else{
-				if (ulazOdServera.readLine().equals("izbacen")) {
-					gp.obavestiOIspadanju();
-					status = false;
-				} 
-				}
-				
-				//dobija poruku od servera da li je partija gotova
-				if (ulazOdServera.readLine().equals("kraj")) {
-					kraj = true;
-					continue;
-				}
-			
-			//salje serveru odgovor za izbacivanje
-			if (status) {
-				while (!gp.izbacivanjeFlag) {
-					System.out.println();
-				}
-				izlazKaServeru.println(gp.vratiIzborZaIzbacivanje());
-			}
-			
-			//dobija odgovor ko je izbacen
-			gp.obavestiOIzglasavanju(ulazOdServera.readLine());
-			
-				//dobija poruku da li je igrac izbacen
-				if (ulazOdServera.readLine().equals("izbacen")) {
-					gp.obavestiOIspadanju();
-					status = false;
-				}
-			
-			//dobija poruku da li je sad kraj partije
-			if (ulazOdServera.readLine().equals("kraj")) {
-				kraj = true;
-			}
-			//ako nije, sve ispocetka
-			}
-			
-			//ako jeste kraj partije, izbaci prozor za ponovno biranje partije
-			pk = new ProzorZaKraj();
-			pk.setVisible(true);
-			//ispisi ko je pobedio
-			pk.ispisi(ulazOdServera.readLine());
-			gp.dispose();
-			while (!pk.flag) {
-				System.out.println();
-			}
-			pk.flag = false;
-			pk.dispose();
-		}
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
+			System.out.println("SERVER NIJE POKRENUT");
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("SERVER NIJE POKRENUT");
 		}
-		
+
 	}
 
 }
--- src/gui/PocetniProzor.java
+++ src/gui/PocetniProzor.java
@@ -1,29 +1,32 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.Frame;
+
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
-import java.awt.Color;
 import java.awt.SystemColor;
-import javax.swing.UIManager;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JToggleButton;
+import javax.swing.JRadioButton;
 
 public class PocetniProzor extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField imeField;
+	public JPanel contentPane;
+	public JTextField imeField;
 	public boolean flag = false;
+	public boolean uspesnoUlogovan = false;
+	public JTextField passwordField;
+	public JRadioButton rdbtnRegistrujMe;
 
 	/**
 	 * Create the frame.
@@ -36,41 +39,80 @@ public class PocetniProzor extends JFrame {
 		contentPane.setBackground(SystemColor.textHighlight);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblDobrodoliUnesiteIme = new JLabel("Dobro do\u0161li! Unesite ime:");
+		contentPane.setLayout(new MigLayout("", "[1.00px,grow][84.00px][226px][grow]",
+				"[grow][25px][20px][20px][43.00px][46.00px][16.00px][10px:n,grow]"));
+
+		JLabel lblDobrodoliUnesiteIme = new JLabel("Dobro do\u0161li!");
 		lblDobrodoliUnesiteIme.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblDobrodoliUnesiteIme.setBounds(102, 38, 252, 23);
-		contentPane.add(lblDobrodoliUnesiteIme);
-		
+		contentPane.add(lblDobrodoliUnesiteIme, "cell 1 0 2 1,alignx center,growy");
+
+		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblUsername, "cell 1 2,growx,aligny center");
+
 		imeField = new JTextField();
-		imeField.setBounds(102, 89, 226, 20);
-		contentPane.add(imeField);
+		contentPane.add(imeField, "cell 2 2,growx,aligny top");
 		imeField.setColumns(10);
-		
-		JCheckBox chckbxPrviPutIgram = new JCheckBox("Prvi put igram");
-		chckbxPrviPutIgram.setBackground(SystemColor.textHighlight);
-		chckbxPrviPutIgram.setBounds(101, 116, 211, 23);
-		contentPane.add(chckbxPrviPutIgram);
-		
-		JButton btnOk = new JButton("OK");
+
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		contentPane.add(lblPassword, "cell 1 3,growx,aligny center");
+
+		JButton btnOk = new JButton("Log-In");
 		btnOk.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				flag = true;
-				imeField.setEditable(false);
+				System.out.println(proveriDaLiSuPoljaValidna());
+				if (proveriDaLiSuPoljaValidna()) {
+					flag = true;
+					imeField.setEditable(false);
+					passwordField.setEditable(false);
+				} else {
+					JOptionPane.showMessageDialog(null, "Polje ne sme biti prazno, niti sme sadrzati znak \";\"");
+				}
+
 			}
 		});
-		btnOk.setBounds(173, 159, 89, 23);
-		contentPane.add(btnOk);
-		
-		JLabel lblSaekajteOstaleIgrae = new JLabel("Sa\u010Dekajte ostale igra\u010De...");
-		lblSaekajteOstaleIgrae.setBounds(159, 204, 158, 14);
-		contentPane.add(lblSaekajteOstaleIgrae);
+
+		passwordField = new JTextField();
+		passwordField.setColumns(10);
+		contentPane.add(passwordField, "cell 2 3,growx,aligny top");
+
+		rdbtnRegistrujMe = new JRadioButton("REGISTRUJ ME");
+		rdbtnRegistrujMe.setBackground(SystemColor.textHighlight);
+		contentPane.add(rdbtnRegistrujMe, "cell 2 4");
+		contentPane.add(btnOk, "cell 1 5 2 1,grow");
 	}
-	
+
 	public String vratiIme() {
+
 		flag = false;
 		return imeField.getText();
 	}
-	
+
+	public String vratiPassword() {
+
+		flag = false;
+		return passwordField.getText();
+	}
+
+	public void obrisiPolja() {
+		imeField.setText("");
+		imeField.setEditable(true);
+		passwordField.setText("");
+		passwordField.setEditable(true);
+	}
+
+	public boolean proveriDaLiSuPoljaValidna() {
+
+		String ime = imeField.getText();
+		String pass = passwordField.getText();
+
+		// provera validnosti unetih podataka
+		if (ime != null && ime != "" && ime != " " && !ime.contains(";") && pass != null && pass != "" && pass != " "
+				&& !pass.contains(";")) {
+			return true;
+		}
+		return false;
+	}
+
 }
