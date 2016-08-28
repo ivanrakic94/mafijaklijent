3cbbeb517f448222a21b17ba4fa2877dc8329167
nemanja.vukic <n.vukic94@gmail.com>
2016-08-29 06:15:42 +0800

    Omoguceno pokretanje vise partija istovrremeno

--- src/MafijaKlijent.java
+++ src/MafijaKlijent.java
@@ -6,8 +6,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.LinkedList;
+
 import javax.swing.*;
 
 public class MafijaKlijent {
@@ -205,3 +208,4 @@ public class MafijaKlijent {
 	}
 
 }
+
